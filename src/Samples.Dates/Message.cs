56f4825c65607f25611288cd9e005b5312e77666
Martin Ortega <lynxestudio@outlook.com>
2023-10-09 16:36:34 -0400

    Add source code to project

---
+++ src/Samples.Dates/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Samples.Dates
+{
+	public class Message
+	{
+		public string Title
+		{
+			private set; get;
+		}
+		public DateTime DateCreated
+		{
+			//get { return DateTime.Today; }
+			get { return DateTime.Now; }
+		}
+		public string Description
+		{
+			set; get;
+		}
+		private Message() { }
+		public Message(string title)
+		{
+			Title = title;
+		}
+	}
+}
